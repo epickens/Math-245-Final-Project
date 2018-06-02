223e0dbaab37e2dce9a1ed4a7a716bea6147840c
Elliot <e99pickens@gmail.com>
2018-06-02 17:24:06 -0500

    new stuff

--- Final_Raw_Code.R
+++ Final_Raw_Code.R
@@ -4,8 +4,37 @@ library(pscl)
 library(FactoMineR)
 library(MASS)
 library(survey)
+library(ggformula)
+library(sandwich)
+
 nba <- read_excel("UPDATEDTEAMDATA.xlsx")
 scatterplotMatrix(nba)
+
+#Some EDA
+
+plot(sort(nba$Playoff_Wins))
+
+#Regular Poisson
+
+reg.pois <- glm(Playoff_Wins ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf + Def_Rtg + Off_Eff, family = "poisson", data = nba)
+summary(reg.pois)
+#The regular model doesn't look rediculous, but it does seem a little bit funky (notice the ConfWest signficance, and that nearly
+#half of the features appear to be ar least somewhat important)
+
+#Quasi Poisson
+
+mod.quasi <- glm(Playoff_Wins ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf + Def_Rtg + Off_Eff, family = quasipoisson, data = nba)
+summary(mod.quasi)
+
+#Negative Binomial 
+
+mod.nb <- glm.nb(Playoff_Wins ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf + Def_Rtg + Off_Eff, data = nba)
+summary(mod.nb)
+
+#Still similar to the previous regular models
+
+#Now we will take a look at some models that can hopefully handle the large number of zeros in our data
+#The model types that we will investigate are the hurdle and zero inflated model
 
 #testing zero inflated model
 
@@ -71,14 +100,26 @@ mod.zero <- zeroinfl(Playoff_Wins ~ FT + AST + STL + BLK + TOV + PF + Conf + Def
 summary(mod.zero)
 zero.basic <- zeroinfl(Playoff_Wins ~ 1, data = nba, dist = "poisson")
 
+#stepwise selection
 zero.step <- stepAIC(zero.basic, scope = list(lower = ~ 1, upper = ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf + Def_Rtg + Off_Eff), direction = "both", k = log(nrow(prin_data)))
 summary(zero.step)
 
 #No off or def rating included 
-zero.full <- hurdle(Playoff_Wins ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf, data = nba, dist = "negbin")
+#the four lines below are error prone 
+zero.full <- zeroinfl(Playoff_Wins ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf, data = nba, dist = "poisson")
 summary(zero.full)
 zero.BK <-stepAIC(zero.full, scope = list(lower = ~ 1, upper = ~ TwoP + ThreeP + ORB + DRB + FT + AST + STL + BLK + TOV + PF + Conf + Def_Rtg + Off_Eff), direction = "backward", k = log(nrow(prin_data)))
 summary(zero.BK)
+
+#quick manual fit (for some function testing)
+zero.test <- zeroinfl(Playoff_Wins ~ FT + ThreeP + STL + TOV, data = nba, dist ="poisson")
+summary(zero.test)
+
+#fit testing for: zero.BK zero.step hur.BK hur.step
+
+AIC <- c(AIC(zero.BK), AIC(zero.step), AIC(hur.BK), AIC(hur.step))
+plot(AIC)
+
 
 
 #More PCA stuff (begin using PCA to create a predictive model)
